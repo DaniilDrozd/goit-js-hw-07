fedb1af717081323726e1f9333268f3c0cb03582
DaniilDrozd <124815236+DaniilDrozd@users.noreply.github.com>
2023-06-19 20:45:41 +0300

    1 hw finish with out modal.

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -8,8 +8,8 @@ const galleryMarkUp = document.querySelector('.gallery');
 const imagesMarkup = createItemsMarkup(galleryItems);
 galleryMarkUp.insertAdjacentHTML("beforeend",imagesMarkup);
 
-function createItemsMarkup(item){
-return galleryItems .map(({preview, original, description}) => {
+function createItemsMarkup(items){
+return items.map(({preview, original, description}) => {
     return  ` 
     <a class="gallery__link" href="${preview}">
     <img src="${preview}" class="gallery__image" data-source="${original}" alt="${description}">
@@ -21,17 +21,13 @@ return galleryItems .map(({preview, original, description}) => {
 // galleryItems.forEach(galleryItems => galleryItems.addEventListener('click',displayImage));
 const onContainerClick = (e) => {
 e.preventDefault();
-if (e.target.classlist.contains("gallery")) return;
+if (e.target.classList.contains("gallery")) return;
 const source = e.target.dataset.source;
 const modal = basicLightbox.create(
-    ` <img scr = "${evt.target.dataset.source} " width = "800" height = "600">`,      
-)
+    `<img src="${source}" width="800" height="600">`      
+);
 modal.show();
 };
-function onEsckeyPress(evt){
-    if (evt.code === "Escape"){
-        modal.close();
-    }
-};
+
 
 galleryMarkUp.addEventListener("click", onContainerClick);
