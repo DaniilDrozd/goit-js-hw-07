aafabf098020a6f9ee704dc0fd621583900721ea
DaniilDrozd <124815236+DaniilDrozd@users.noreply.github.com>
2023-06-20 17:55:30 +0300

    have problems

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryMarkUp = document.querySelector('.gallery');
+const imagesMarkup = createItemsMarkup(galleryItems);
+galleryMarkUp.insertAdjacentHTML("beforeend",imagesMarkup);
+function createItemsMarkup(items){
+    return items.map(
+        ({description,original,preview}) =>{
+            `li class = "gallery__item">
+            <a class= "gallery__link" href=${original}>
+            <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+          />
+        </a>
+      </li>`
+ })
+ .join("");
+ 
+//  galleryMarkUp.insertAdjacentHTML("beforeend", imagesMarkup);
+
+
+let lightbox = new SimpleLightbox(".gallery a", {
+    captions: true,
+    captionsData: "alt",
+    captionDelay: 250,
+  }); 
+
+}
